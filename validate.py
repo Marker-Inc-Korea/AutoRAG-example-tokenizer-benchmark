0e5e4d278695114e950a9a40cdd88531e0276e8b
jeffrey <vkefhdl1@gmail.com>
2024-08-10 18:08:36 +0800

    init commit

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import os
 
 import autorag
 import click
-from autorag.evaluator import Evaluator
+from autorag.validator import Validator
 from dotenv import load_dotenv
 
 from llama_index.embeddings.upstage import UpstageEmbedding
@@ -17,14 +17,11 @@ data_path = os.path.join(root_path, 'data')
               default=os.path.join(data_path, 'qa_v4.parquet'))
 @click.option('--corpus_data_path', type=click.Path(exists=True),
               default=os.path.join(data_path, 'ocr_corpus_v3.parquet'))
-@click.option('--project_dir', type=click.Path(exists=False), default=os.path.join(root_path, 'benchmark'))
-def main(config, qa_data_path, corpus_data_path, project_dir):
+def main(config, qa_data_path, corpus_data_path):
     load_dotenv()
     autorag.embedding_models['upstage_embed'] = autorag.LazyInit(UpstageEmbedding)
-    if not os.path.exists(project_dir):
-        os.makedirs(project_dir)
-    evaluator = Evaluator(qa_data_path, corpus_data_path, project_dir=project_dir)
-    evaluator.start_trial(config)
+    validator = Validator(qa_data_path, corpus_data_path)
+    validator.validate(config)
 
 
 if __name__ == '__main__':
